3db8f11b9aec15eb08f836223d169350ece9024c
Mirza Hammad Baig <mirzahammadbaig56@gmail.com>
2026-08-08 11:41:41 +0500

    all setup config

--- src/app.js
+++ src/app.js
@@ -4,10 +4,6 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.on('error', (err) => {
-  console.error('Express app error:', err);
-});
-
 app.use(cors({
   origin: process.env.FRONTEND_URL, // Replace with your frontend URL
   credentials: true, // Allow cookies to be sent
@@ -17,5 +13,12 @@ app.use(express.json({limit: '16kb'})); // Parse JSON bodies with a limit of 16K
 app.use(express.urlencoded({ extended: true, limit: '16kb' })); // Parse URL-encoded bodies with a limit of 16KB
 app.use(express.static('public')); // Serve static files from the 'public' directory
 app.use(cookieParser()); // Parse cookies
+
+// import Routers
+import userRouter from './routes/user.routes.js';
+
+// Routers decalaration
+app.use("/api/v1/users", userRouter);
+
 
 export default app;
