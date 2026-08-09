9cd1c9f2a668516b4337dc46f6840344367797ce
Mirza Hammad Baig <mirzahammadbaig56@gmail.com>
2026-08-09 20:11:35 +0500

    user controller logic - 1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,12 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "Hammad's first controller ok",
-  });
+  const { username, email, fullname, password } = req.body;
+  if(
+    [username, email, fullname, password].some((field) => !field)
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 });
 
 export { registerUser };
