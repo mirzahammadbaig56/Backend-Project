e676e7b42838b63a5125dfbc1881fe1f85e6518d
Mirza Hammad Baig <mirzahammadbaig56@gmail.com>
2026-08-27 18:04:03 +0500

    fix: import Video model in comment.controller.js for comment functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -4,6 +4,7 @@ import ApiError from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { commentZodSchema } from "../validators/comment.validator.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
